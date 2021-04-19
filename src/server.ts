fc1c86d36ece42754a87f920308a1c45c26e31b1
Alessandro Lins <aflinsjr@icloud.com>
2021-04-18 21:44:43 -0300

    Iniciando com BD / Migrations

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Cadastro de Usuários'});
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀🚀 Server started on port 3333');
